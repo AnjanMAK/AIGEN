26d7e286646606544be575bf31cc92b7876b7cba
AnjanMAK <anjan.merugumala@hotmail.com>
2024-05-08 20:26:39 +0100

    Add files via upload

---
+++ Sample.py
@@ -0,0 +1,5 @@
+#Read 2 numbers from the keyboard and print sum and product
+n1 = int(input('Enter First Number:'))
+n2 = int(input('Enter second Number:'))
+print('The Sum:',n1+n2)
+print('The Product:', n1*n2)
